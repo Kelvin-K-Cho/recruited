d18e6696c11fb14af58b5fc3229f8a09c246b218
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-02-19 00:07:41 -0800

    Implemented Auth; Refactored for production level deployment

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,4 +12,14 @@ module.exports = (app) => {
     "/auth/google/callback",
     passport.authenticate("google")
   );
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+
 };
