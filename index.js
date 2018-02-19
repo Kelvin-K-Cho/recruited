0c4f52d4ebb6e730cb74bdfc0d22a39760afbcab
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-02-18 16:29:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-//sup
