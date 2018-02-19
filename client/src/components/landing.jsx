8dab72d52d8bd735165001fcb042d0eecba3c04a
Edward <edwardcheng148@gmail.com>
2018-02-19 11:21:17 -0800

    added minimum features to landing page

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -8,7 +8,24 @@ class Landing extends React.Component {
   render() {
     return (
       <div className="main-landing-div">
-        <div className="landing-div">My New Landing Page Is Better</div>
+        <div className="navigation-div">
+          <h1 className="landing-header">≋R≋E≋C≋R≋U≋I≋T≋E≋D≋</h1>
+        </div>
+        <div className="session-div-logged-out">
+          <p className="first-text">
+            Start By Doing What Is Necessary,
+          </p>
+          <p className="second-text">
+            Then What Is Possible, And Suddenly
+          </p>
+          <p className="third-text">
+            You Are Doing The Impossible.
+          </p>
+          <p className="fourth-text">
+            Join The Work Force Today!
+          </p>
+          <button className="button-to-sign-up">Sign Up Now</button>
+        </div>
       </div>
     );
   }
