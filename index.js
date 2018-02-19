1b6a7b2d362b00ea065217e70f5612fb94ef4289
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-02-18 18:00:44 -0800

    Implement OAuth

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ bye: "buddy" });
+// });
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-//Apple
