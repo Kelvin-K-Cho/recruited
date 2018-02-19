65288d9da51fe1b1eb0ec9eaf8724ddc4540eba8
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-02-19 00:10:06 -0800

    Deploy to heroku

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: "824061176810-i5omfjsdg1suluvmt028glqvebtd8iou.apps.googleusercontent.com",
+  googleClientSecret: "bnpEv1DyQ16QcT2IaSXyK97q",
+  mongoURI: "mongodb://admin:tekpassword@ds241658.mlab.com:41658/recruited-dev",
+  cookieKey: "sdfuhwieurhlkkabdsvaksvbiqwrpasdfajksdfkaghshsdjwquryzovnzoenklsdabvnq"
+};
