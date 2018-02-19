2144fa8cd2c33769966e257e1a1e2ab5ea78967f
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-02-19 10:59:06 -0800

    Finish Auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { connect } from "react-redux";
+
 
 class Header extends React.Component {
+
+  renderContent(){
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+        <li>
+          <a href="/api/logout">Logout</a>
+        </li>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
         Header
-        <a>Login with Google</a>
+        {this.renderContent()}
       </div>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
