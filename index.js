41f7406ac71bbd147aade8d225ceefb96754f4d4
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-02-18 22:28:47 -0800

    Refactor code and setup DB

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
 const express = require("express");
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./services/passport");
+require("./models/User");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send({ bye: "buddy" });
-// });
-
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-    console.log(accessToken);
-  })
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
