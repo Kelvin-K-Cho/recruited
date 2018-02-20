312d47b4467474a456d4d2105520b3a6e8be96dc
Edward <edwardcheng148@gmail.com>
2018-02-20 10:11:28 -0800

    added auth and protected routes

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        Header
+        <a>Login with Google</a>
+      </div>
+    );
+  }
+}
+
+export default Header;
