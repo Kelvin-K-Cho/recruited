dc16d343d9ee5585132d5ae268b266faf82acee0
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-02-21 09:25:30 -0800

    Comment job_index out

--- client/src/components/jobs/job_listing/Dashboard.js
+++ client/src/components/jobs/job_listing/Dashboard.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../../styles/jobs.css';
-import JobsIndexContainer from './jobs_index_container';
+// import JobsIndexContainer from './jobs_index_container';
 import JobList from '../JobList';
 
 const Dashboard = () => {
   return (
     <div className="logged-in-main-div">
       <p className="button-to-create-job">
-        <Link className="link-to-create-job" 
+        <Link className="link-to-create-job"
           to="/jobs/new">
           New Job Form
         </Link>
       </p>
       <div className="logged-in-header">
-      All Jobs - Sorted By :  
+      All Jobs - Sorted By :
       <span className="logged-in-recent">
-      [ Recent ] 
+      [ Recent ]
       </span>
       </div>
         <JobList />
