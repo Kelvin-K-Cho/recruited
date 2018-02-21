236fcba679473f1c0c78f100f29574e5aa0d2764
locphan2207 <locphan2207@gmail.com>
2018-02-21 11:50:04 -0800

    add file converter & file review

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import * as actions from '../actions';
+
 import { ProtectedRoute, AuthRoute } from '../util/route_util.jsx';
+import * as actions from '../actions';
 import Landing from './landing/landing.jsx';
 import '../styles/reset.css';
 import '../styles/main.css';
-
-
 import Header from "./header.jsx";
 // import Landing from './Landing';
 import Dashboard from "./jobs/job_listing/Dashboard";
@@ -23,18 +22,18 @@ class App extends React.Component {
 
   render() {
     return (
-        <div className="outer-container-div">
-          <BrowserRouter>
-            <div className="inner-container-div">
-              <Header />
-              <Route exact path="/" component={Landing}/>
-              <Route exact path="/jobs" component={Dashboard}/>
-              <Route path="/jobs/:id" component={JobShow}/>
-              <Route exact path="/jobs/new" component={JobNew}/>
-            </div>
-          </BrowserRouter>
-        </div>
-      );
+      <div className="outer-container-div">
+        <BrowserRouter>
+          <div className="inner-container-div">
+            <Header />
+            <Route exact path="/" component={Landing}/>
+            <Route exact path="/jobs" component={Dashboard}/>
+            <Route path="/jobs/:id" component={JobShow}/>
+            <Route exact path="/jobs/new" component={JobNew}/>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
