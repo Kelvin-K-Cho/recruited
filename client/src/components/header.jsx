eac72674937cc704d26ec6ee61fcd7ce322e47ba
locphan2207 <locphan2207@gmail.com>
2018-02-21 01:19:26 -0800

    move resume button to header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { connect } from "react-redux";
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {openResume: false};
+  }
 
   renderContent(){
     switch (this.props.auth) {
@@ -28,9 +32,52 @@ class Header extends React.Component {
     }
   }
 
+  closeResumeModal(e) { //close modal when clicking outside
+    if (e.target === document.getElementsByClassName('modal-screen')[0]) {
+      this.setState({openResume: false});
+    }
+  }
+
+  renderResumeModal() {
+    if (!this.state.openResume) return (<div></div>);
+    return (
+      <div onClick={(e)=> this.closeResumeModal(e)}
+        className="modal-screen">
+        <form className="resume-form">
+          <img onClick={()=> this.setState({openResume: false})}
+            className="close-button"
+            src="https://www.materialui.co/materialIcons/navigation/close_grey_192x192.png">
+          </img>
+          <div>Upload your resume (.doc, .txt)</div>
+            <input id="resume-input" type="file"/>
+          <input onClick={() => this.handleFile()}
+            type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+
+  handleFile() {
+    const resume = document.getElementById('resume-input').files[0];
+    console.log(resume);
+    if (resume) {
+      let reader = new FileReader();
+      reader.onload = (e) => {  // initialize event on reader
+        // save result into variable
+        console.log(e.target.result);
+      };
+      reader.readAsText(resume);  // start reading, can only read .txt
+      // should call action to save variable to database
+    }
+  }
+
   render() {
     return (
       <div className="outer-navigation-div">
+        <button className="resume-button" onClick={()=> this.setState({openResume: true})}>
+            Upload Resume
+        </button>
+        <div>{this.renderResumeModal()}</div>
         {this.renderContent()}
       </div>
     );
