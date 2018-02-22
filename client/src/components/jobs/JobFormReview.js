6c4a881083bb160cbd129ed1a5962834d7df5fd5
Edward <edwardcheng148@gmail.com>
2018-02-21 17:14:43 -0800

    added styling to new job and new job review

--- client/src/components/jobs/JobFormReview.js
+++ client/src/components/jobs/JobFormReview.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import formFieldsTextArea from './formFieldsTextArea';
+import formFieldsRemainder from './formFieldsRemainder';
 import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
@@ -9,9 +11,31 @@ const JobFormReview = ({ onCancel, formValues, submitJob, history }) => {
 
   const reviewFields = _.map(formFields, ({name, label}) => {
     return (
-      <div key={name}>
-        <label>{label}</label>
-        <div>
+      <div className="review-main-div" key={name}>
+        <label className="review-label">{label}</label>
+        <div className="review-value">
+          {formValues[name]}
+        </div>
+      </div>
+    );
+  });
+  
+  const reviewFieldsTextArea = _.map(formFieldsTextArea, ({name, label}) => {
+    return (
+      <div className="review-main-div" key={name}>
+        <label className="review-label">{label}</label>
+        <div className="review-value-textarea">
+          {formValues[name]}
+        </div>
+      </div>
+    );
+  });
+  
+  const reviewFieldsRemainder = _.map(formFieldsRemainder, ({name, label}) => {
+    return (
+      <div className="review-main-div" key={name}>
+        <label className="review-label">{label}</label>
+        <div className="review-value">
           {formValues[name]}
         </div>
       </div>
@@ -19,11 +43,13 @@ const JobFormReview = ({ onCancel, formValues, submitJob, history }) => {
   });
 
   return (
-    <div>
-      <h5>Confirm your job listing</h5>
+    <div className="outer-review-form-div">
+      <h5 className="confirm-job-header">Confirm Your Job Listing</h5>
       {reviewFields}
-      <button onClick={onCancel}>Back</button>
-      <button onClick={() => submitJob(formValues, history)}>Submit</button>
+      {reviewFieldsTextArea}
+      {reviewFieldsRemainder}
+      <button className="back-button-new-job" onClick={onCancel}>Back</button>
+      <button className="submit-button-new-job" onClick={() => submitJob(formValues, history)}>Submit</button>
     </div>
   );
 };
