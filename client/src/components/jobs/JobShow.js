56f1f1421ca5e7c33fe9e29b939a6e4f6e4dae6c
locphan2207 <locphan2207@gmail.com>
2018-02-21 20:48:16 -0800

    move resume button, make resume database related to job

--- client/src/components/jobs/JobShow.js
+++ client/src/components/jobs/JobShow.js
@@ -1,13 +1,83 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchJob } from '../../actions/index';
-
 import {Link} from 'react-router-dom';
+import mammoth from 'mammoth';
+
+
+import {submitResume} from '../../actions';
+
 
 class JobShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {openResume: false};
+    this.resumeText;
+    this.resumeHTML;
+  }
 
   componentDidMount() {
     this.props.fetchJob(this.props.match.params.id);
+  }
+
+  closeResumeModal(e) { //close modal when clicking outside
+    if (e.target === document.getElementsByClassName('modal-screen')[0]) {
+      this.setState({openResume: false});
+    }
+  }
+
+  renderResumeModal() {
+    if (!this.state.openResume) return (<div></div>);
+    return (
+      <div onClick={(e)=> this.closeResumeModal(e)}
+        className="modal-screen">
+        <form className="resume-form">
+          <img onClick={()=> this.setState({openResume: false})}
+            className="close-button"
+            src="https://www.materialui.co/materialIcons/navigation/close_grey_192x192.png">
+          </img>
+          <div>Upload your resume (.doc, .docx)</div>
+            <input onChange={() => this.handleFile()} id="resume-input" type="file"/>
+          <input onClick={() => this.submitFile()}
+            type="submit" value="Submit" />
+          <div id="file-review">File Review</div>
+        </form>
+      </div>
+    );
+  }
+
+  submitFile() {
+    console.log("submitting");
+    this.props.submitResume({
+      resumeText: this.resumeText,
+      resumeHTML: this.resumeHTML,
+      _job: this.props.match.params.id
+    });
+  }
+
+  handleFile() {
+    const resume = document.getElementById('resume-input').files[0];
+    const review = document.getElementById('file-review');
+
+    if (resume) {
+      let reader = new FileReader();
+      reader.onload = (e) => {  // initialize event on reader
+        // save result into arrayBuffer
+        const arrayBuffer = e.target.result;
+        // Using mammoth to convert arrayBuffer into Raw Text
+        mammoth.extractRawText({
+          arrayBuffer: arrayBuffer
+        }).then((result) => {this.resumeText = result.value;});
+        // Using mammoth to convert arrayBuffer into inner HTML
+        mammoth.convertToHtml({
+          arrayBuffer: arrayBuffer
+        }).then((result) => {
+          this.resumeHTML = result.value;
+          review.innerHTML = result.value;
+        });
+      };
+      reader.readAsArrayBuffer(resume);  // start reading
+    }
   }
 
   renderJob(){
@@ -37,6 +107,10 @@ class JobShow extends React.Component {
     if (!this.props.job) return (<div></div>);
     return (
       <div className="job-show-page-outer-div">
+        <button className="resume-button" onClick={()=> this.setState({openResume: true})}>
+          Upload Resume
+        </button>
+        <div>{this.renderResumeModal()}</div>
         {this.renderJob()}
       </div>
     );
@@ -50,7 +124,8 @@ function mapStateToProps(state, ownProps) {
 function mapDispatchToProps(dispatch, ownProps) {
   console.log(ownProps.match.params.id);
   return {
-    fetchJob: (id) => dispatch(fetchJob(id))
+    fetchJob: (id) => dispatch(fetchJob(id)),
+    submitResume: (values) => dispatch(submitResume(values))
   };
 }
 
