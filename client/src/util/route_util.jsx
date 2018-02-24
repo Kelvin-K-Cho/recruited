efaed5cadd00f6cb35dfb11532eb3f83e35e067d
Edward <edwardcheng148@gmail.com>
2018-02-24 13:19:03 -0800

    added auth route

--- client/src/util/route_util.jsx
+++ client/src/util/route_util.jsx
@@ -25,5 +25,5 @@ const Auth = ({ loggedIn, path, component: Component }) => (
   />
 );
 
-export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
-export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
+export const AuthRoute = connect(mapStateToProps)(Auth);
+export const ProtectedRoute = connect(mapStateToProps)(Protected);
